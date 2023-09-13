797c45c8e81a63e05c33a281b69e3767745f9d14
joel-gr <joel.garcia.ramirez@ciclosmontecastelo.com>
2023-09-13 13:01:18 +0200

    descripcion
    descripcion

---
+++ Ejemplo1_dia13/Ejemplo1_dia13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_dia13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
